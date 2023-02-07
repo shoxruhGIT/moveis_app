02dc365538e308de891462f74f058f46c706f593
dev <2006@gmail.com>
2023-02-07 23:05:44 +0500

    yangi

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
-let moviesList = document.querySelector('.moviesList')
+let moviesList = document.querySelector('.moviesList'),
+API_KEY = '3bf3e15',
+page = localStorage.getItem('page'),
+movie = localStorage.getItem('movieName'),
+poisk = document.querySelector('.poisk'),
+search = document.querySelector('.search'),
+pageBtn = document.querySelectorAll('#pageBtn'),
+form = document.querySelector('.form')
 
 
-fetch(`http://www.omdbapi.com/?s=Batman&page=1&apikey=3bf3e15`)
+search.value = localStorage.getItem('movieName')
+
+function validationPage(){
+  for(let pageNum of pageBtn){
+    fetch(`http://www.omdbapi.com/?s=${movie}&page=${+pageNum.innerHTML}&apikey=${API_KEY}`)
+    .then(res=> res.json())
+    .then(data=> {
+      // console.log(data);
+
+      if(data.Response == 'False'){
+        pageNum.style.display = 'none'
+
+      }
+    })
+  }
+}
+
+validationPage()
+
+fetch(`http://www.omdbapi.com/?s=${movie}&page=${page}&apikey=${API_KEY}`)
 .then(res=> res.json())
 .then(data=> {
     for(let movie of data.Search){
@@ -22,5 +48,32 @@ fetch(`http://www.omdbapi.com/?s=Batman&page=1&apikey=3bf3e15`)
       card.appendChild(title)
       card.appendChild(year)
       moviesList.appendChild(card)
+
     }
+
+})
+.catch(err=>{
+  let error = document.createElement('h1')
+
+  error.innerHTML = 'Sorry movie not found'
+  moviesList.appendChild(error)
+
+})
+
+function paginationPage (e) {
+  localStorage.setItem('page', e.innerHTML)
+}
+
+
+poisk.addEventListener('click', ()=>{
+  if(localStorage.getItem('movieName') != search.value){
+    localStorage.setItem('page', 1)
+  }
+  else{
+    localStorage.setItem('page', e.innerHTML)
+  }
+
+  
+
+  localStorage.setItem('movieName', search.value)
 })
